4cda7873a489eb20d388a4b2c83d0ddc3039a190
Alexander Black <a84366973@china.huawei.com>
2024-07-30 18:19:34 +0100

    Quartz sync: Jul 30, 2024, 6:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "alexblck.github.io/runeterra-quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
